7a81b4b42ba3352792fb72727e7b719a86f7e034
methu45 <110477491+methu45@users.noreply.github.com>
2022-08-05 18:52:26 +0530

    Update alive-test.js

--- plugins/alive-test.js
+++ plugins/alive-test.js
@@ -1,27 +1,27 @@
 const evt = require('../events')
 const config = require('../config');
 
-evt.getCMD({pattern: 'alive$', fromMe: false, deleteCommand: false, desc: "get songs froom youtube"}, (async (message, match) => {
+evt.getCMD({pattern: 'start$', fromMe: false, deleteCommand: false, desc: "get songs froom youtube"}, (async (message, match) => {
    var alivemsg = '```👋 Hey there, I\'m alive now``` \n\n_Version:_ ```' + config.VERSION + '```\n\n _POWERED BY KAVIYAAH_\n'
 		
    const alivebuttond = [
-    {buttonId: '.aliveimage',  buttonText: {displayText: 'ALIVE 1'}, type: 1},
-    {buttonId: '.alivetext', buttonText: {displayText: 'ALIVE 2'}, type: 1},
+    {buttonId: '.aliveimage',  buttonText: {displayText: '🤖MENU'}, type: 1},
+    {buttonId: '.alivetext', buttonText: {displayText: '⚙️INFO'}, type: 1},
     ]
 
- await message.client.sendMessage(message.jid, { image: {url: 'https://telegra.ph/file/3217c19d381ae34b38012.jpg' }, caption: alivemsg , footer: 'Queen Diana' , buttons: alivebuttond , headerType: 4 } , { quoted: message.data } )
+ await message.client.sendMessage(message.jid, { image: {url: 'https://telegra.ph/file/8086c81d1d79f63c7cc73.jpg' }, caption: alivemsg , footer: '©Axzi-X' , buttons: alivebuttond , headerType: 4 } , { quoted: message.data } )
 
 }));
 
 evt.getCMD({pattern: 'aliveimage$', fromMe: false, deleteCommand: false, desc: "get songs froom youtube"}, (async (message, match) => {
-   var alivemsg = '```👋 Hey there, I\'m alive now``` \n\n_Version:_ ```' + config.VERSION + '```\n\n _POWERED BY KAVIYAAH_\n'
+   var alivemsg = '```👋 Hey there, I\'m alive now``` \n\n_Version:_ ```' + config.VERSION + '```\n\n ©Axzi-X'
 
- await message.client.sendMessage(message.jid, { image: {url: 'https://telegra.ph/file/3217c19d381ae34b38012.jpg' }, caption: alivemsg  } , { quoted: message.data } )
+ await message.client.sendMessage(message.jid, { image: {url: 'https://telegra.ph/file/8086c81d1d79f63c7cc73.jpg' }, caption: alivemsg  } , { quoted: message.data } )
 
 }));
 
 evt.getCMD({pattern: 'alivetext$', fromMe: false, deleteCommand: false, desc: "get songs froom youtube"}, (async (message, match) => {
-   var alivemsg = '```👋 Hey there, I\'m alive now``` \n\n_Version:_ ```' + config.VERSION + '```\n\n _POWERED BY KAVIYAAH_\n'
+   var alivemsg = '```👋 Hey there, I\'m alive now``` \n\n_Version:_ ```' + config.VERSION + '```\n\n©Axzi-X'
 		
 
  await message.client.sendMessage(message.jid, { text: alivemsg } , { quoted: message.data } )
