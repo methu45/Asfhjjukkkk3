6257ccad0477be7e56c378465f3e5dcdac769728
methu45 <110477491+methu45@users.noreply.github.com>
2022-08-07 15:00:13 +0530

    Create TestAp.js

---
+++ plugins/TestAp.js
@@ -0,0 +1,34 @@
+
+const Neotro = require("../events");
+const Language = require('../language');
+const Lang = Language.getString('ttp');
+const axios = require("axios");
+const conf = require("../config");
+
+
+
+Neotro.getCMD({pattern: '1tp ?(.*)', fromMe: false, desc: Lang.ATTP_DESC}, (async (message, match) => { 
+
+        const Text = match[1]
+    
+        if (match1] === '') return await message.client.sendMessage(message.jid , { text:Lang.NEED_WORDS}, { quoted: message.data })
+        
+        var uri = encodeURI(match[1])
+        await axios.get(`https://open-apis-rest.up.railway.app/api/glowtext?text=${uri}&font_style=lollipop&font_size=m&glow_halo=1&non_trans=false&retina=true&anim_type=pulse`)
+          .then(async (response) => {
+            const {
+              image,
+            } = response.data.data
+    
+            const Get = await axios.get(image, {responseType: 'arraybuffer'})
+    
+
+        })
+        
+        
+        await message.client.sendMessage(
+      message.jid,
+      { image: Buffer.from(Get.data), caption: conf.CPT },
+      { quoted: message.data });
+        
+      }));
