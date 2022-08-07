1138209a4dba05f9bab2d5a9a7f4ce27aab171f2
methu45 <110477491+methu45@users.noreply.github.com>
2022-08-07 11:14:04 +0530

    Update yt-search.js

--- plugins/yt-search.js
+++ plugins/yt-search.js
@@ -4,42 +4,36 @@ const Neotro = require("../events");
 const Language = require('../language');
 const Lang = Language.getString('scrapers');
 
-Neotro.getCMD({pattern: 'yt ?(.*)', fromMe: false, desc: Lang.YT_DESC}, (async (message, match) => { 
+    Neotro.getCMD({pattern: 'yt ?(.*)', fromMe: false, desc: Lang.YT_DESC}, (async (message, match) => { 
 
     if (match[1] === '') return await message.client.sendMessage(message.jid , { text:Lang.NEED_WORDS}, { quoted: message.data });
     
 
-var reply = await message.client.sendMessage(message.jid , { text:Lang.GETTING_VIDEOS}, { quoted: message.data });
-    
-
-
-    
-    const results = await yts(match[1]);
-
-    let result = results.videos;
-      
-    let rows = [];
-      results.all.map((video) => {
-        let obj = {
-          title: video.title,
-          rowId: `play x/65v79 ${video.videoId}`,
-          description: video.description,
+     var reply = await message.client.sendMessage(message.jid , { text:Lang.GETTING_VIDEOS}, { quoted: message.data });
+     const results = await yts(match[1]);
+     let result = results.videos;
+     let rows = [];
+     results.all.map((video) => {
+     let obj = {
+     title: video.title,
+     rowId: `play x/65v79 ${video.videoId}`,
+     description: video.description,
         };
-        rows.push(obj);
+     rows.push(obj);
       });
-      const sections = [
+     const sections = [
         {
-          title: "Videos",
-          rows: rows,
+      title: "Videos",
+      rows: rows,
         },
       ]
 
       const listMessage = {
-        text: "Youtube Search Results",
-        footer: "©Axzi-X",
-        title: "⚡Axzi  Version  2.0",
-        buttonText: "Click Me",
-        sections,
+      text: "Youtube Search Results",
+      footer: "©Axzi-X",
+      title: "⚡Axzi  Version  2.0",
+      buttonText: "Click Me",
+      sections,
       };
 
       await message.client.sendMessage(message.jid, listMessage);
