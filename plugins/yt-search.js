237b2b761c21c2a07987b574068dd90d1e6481a0
methu45 <110477491+methu45@users.noreply.github.com>
2022-08-07 11:07:04 +0530

    7

--- plugins/yt-search.js
+++ plugins/yt-search.js
@@ -13,16 +13,13 @@ var reply = await message.client.sendMessage(message.jid , { text:Lang.GETTING_V
     
 
 
-    try {
-      const results = await yts(match[1]);
-} catch {
-            return await message.client.sendMessage(message.jid,Lang.NOT_FOUND,MessageType.text);
-        }
     
+    const results = await yts(match[1]);
+
+    let result = results.videos;
       
-      let result = results.videos;
-      let rows = [];
-      result.map((video) => {
+    let rows = [];
+      results.all.map((video) => {
         let obj = {
           title: video.title,
           rowId: `play x/65v79 ${video.videoId}`,
