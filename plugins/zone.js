4fad03379f0eb455ce66c48cafec89cb6b053123
methu45 <110477491+methu45@users.noreply.github.com>
2022-08-11 22:44:26 +0530

    Create zone.js

---
+++ plugins/zone.js
@@ -0,0 +1,16 @@
+let Neotro = require('../events');
+let {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+let fs = require('fs');
+let axios = require('axios');
+let got = require("got");
+let Config = require('../config');
+let Language = require('../language');
+let td = Config.WORKTYPE == 'public' ? false : true
+
+   Neotro.getCMD({pattern: 'get ?(.*)', fromMe: false, desc: 'movie zong' }, (async (message, match) => {
+   
+    var webimage = await axios.get(`https://dl.techwizardent.com/496320`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,{ document : { url : webimage.data  } , mimetype : 'mp4' , fileName : 'slmoviezone.mp4' }, {quoted: message.data})
+    
+    }));    
